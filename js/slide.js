c805e4ecf7b9f09b2dc89ad78ae029177c49a44f
tipaknight <tipa-knight@yandex.ru>
2022-01-28 11:01:07 +0500

    first commit

---
+++ js/slide.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+  new ChiefSlider('.slider', {
+    loop: true,
+    autoplay: true,
+    interval: 5000,
+    refresh: true,
+  });
+});
